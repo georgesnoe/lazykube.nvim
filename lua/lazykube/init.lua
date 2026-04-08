7b687dd87203b30d1f43fc27c6c435a3cdad6a74
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-08 18:26:13 +0000

    feat: update default options and comments to lazykube defaults (#2)

--- lua/lazykube/init.lua
+++ lua/lazykube/init.lua
@@ -1,32 +1,32 @@
 local M = {}
 
 M.default_options = {
-	keymap = "<leader>lg",
-	cmd = "gh lazy",
+	keymap = "<leader>lk",
+	cmd = "lazykube",
 	border = "single",
 }
 
 M.options = M.default_options
-local lazygithub = nil
+local lazykube = nil
 
 M.setup = function(options)
 	--- Clone options and replace empty ones with default ones
 	M.options = vim.tbl_deep_extend("keep", options or {}, M.default_options)
 	-- Register command
-	vim.cmd("command! Lazygithub lua require('lazygithub').open()")
-	-- Pressing <leader>lg will open Lazygithub
-	vim.keymap.set("n", M.options.keymap, "<cmd>Lazygithub<CR>", { desc = "Open Lazygithub" })
+	vim.cmd("command! Lazykube lua require('lazykube').open()")
+	-- Pressing <leader>lk will open Lazykube
+	vim.keymap.set("n", M.options.keymap, "<cmd>Lazykube<CR>", { desc = "Open Lazykube" })
 end
 
 M.open = function()
 	local Terminal = require("toggleterm.terminal").Terminal
 
-	-- Close Lazygithub if it's already open, open a new one if it's not
-	if lazygithub and lazygithub:is_open() then
-		lazygithub:close()
-		lazygithub = nil
+	-- Close Lazykube if it's already open, open a new one if it's not
+	if lazykube and lazykube:is_open() then
+		lazykube:close()
+		lazykube = nil
 	else
-		lazygithub = Terminal:new({
+		lazykube = Terminal:new({
 			cmd = M.options.cmd,
 			direction = "float",
 			float_opts = { border = M.options.border },
@@ -35,7 +35,7 @@ M.open = function()
 				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
 			end,
 		})
-		return lazygithub:toggle()
+		return lazykube:toggle()
 	end
 end
 
